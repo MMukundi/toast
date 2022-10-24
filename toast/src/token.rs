8fee16481e2a392d6138af65b247ba69f7801677
Marcel Mukundi <45983298+MMukundi@users.noreply.github.com>
2022-10-24 00:55:23 -0700

    Rename and clean up tokens

--- toast/src/token.rs
+++ toast/src/token.rs
@@ -33,7 +33,7 @@ impl Debug for Operator {
             Self::Div => "/",
             Self::Mod => "%",
         };
-        write!(f,"[Op: {}]",s)
+        write!(f,"[Operator: {}]",s)
     }
 } 
 
@@ -54,72 +54,30 @@ impl TryFrom<char> for Operator {
 impl Bracket {
     pub fn open(&self)->char{
         match self{
-            Self::Angle => '<',
-            Self::Curly => '{',
-            Self::Square => '[',
             Self::Parenthesis => '(',
         }
     }
     pub fn close(&self)->char{
         match self{
-            Self::Angle => '>',
-            Self::Curly => '}',
-            Self::Square => ']',
             Self::Parenthesis => ')',
         }
     }
 }
-#[derive(Debug,Copy, Clone,Eq, PartialEq)]
-pub enum Keyword {
-    Def,
-    Call,
-    Print
-}
-#[derive(Debug, Clone,Eq, PartialEq)]
-pub struct UnknownKeyword(pub String);
-impl FromStr for Keyword {
-    type Err = UnknownKeyword;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "print" => Ok(Self::Print),
-            "call" => Ok(Self::Call),
-            "def" => Ok(Self::Def),
-            _ => Err(UnknownKeyword(s.to_string())),
-        }
-    }
-}
 
-
-
-#[derive(Clone, PartialEq)]
-pub enum Literal {
-    Number(NumericLiteral),
-}
-impl Debug for Literal {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Literal::Number(n) => Debug::fmt(n,f),
-        }
-    }
-}
 #[derive(Clone, PartialEq)]
 pub enum Token {
     Bracket{
         bracket:Bracket,
         state:BracketState,
     },
-    Literal(Literal),
-    Identifier(String),
-    Keyword(Keyword),
+    Number(isize),
     Operator(Operator),
 }
 impl Debug for Token {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Bracket {bracket,state}=>write!(f,"<{:?}{:?}>",state,bracket),
-            Self::Literal (l)=> Debug::fmt(l,f),
-            Self::Identifier(id)=>write!(f, "<[ID]{:?}>",id),
-            Self::Keyword(kw)=>write!(f, "<[KW]{:?}>",kw),
+            Self::Bracket {bracket,state}=>write!(f,"[Bracket: {:?}{:?}]",state,bracket),
+            Self::Number (n)=> write!(f, "[Number: {:?}]",n),
             Self::Operator(op)=>Debug::fmt(op,f),
         }
     }
